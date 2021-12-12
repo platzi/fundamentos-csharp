f18459cfc1fd6091862ec21f3d272ba2c477ba28
RetaxMaster <carlos@retaxmaster.com>
2021-12-11 20:29:40 -0600

    Adicion de Operadores Logicos

---
+++ LogicalOperatorsExample/LogicalOperatorsExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LogicalOperatorsExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            bool value1 = true;
+            bool value2 = true;
+            bool value3 = false;
+
+            // && , ||, !
+
+            bool result = value1 && value2 && value3;
+            Console.WriteLine("The result of the logical AND is: " + result);
+
+            bool result2 = value1 || value2 || value3;
+            Console.WriteLine("The result of the logical OR is: " + result2);
+
+            bool result3 = !value1;
+            Console.WriteLine("The result of the logical ! is: " + result3);
+        }
+    }
+}
