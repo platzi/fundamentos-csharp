a30a96ca66c147444f58a232077185be4fa3aca4
RetaxMaster <carlos@retaxmaster.com>
2021-12-11 20:00:40 -0600

    Adicion de StringExamples

---
+++ variables/StringExample/StringExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name? Please Write your full name");
+            string fullUserName = Console.ReadLine();
+
+            Console.WriteLine("hello " + fullUserName + ", Welcome to Platzi");
+        }
+    }
+}
