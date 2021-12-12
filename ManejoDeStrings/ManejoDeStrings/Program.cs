358d759bd37574ddc1d6623783b3d62a4fe08edb
RetaxMaster <carlos@retaxmaster.com>
2021-12-11 20:09:27 -0600

    Adicion de ManejoDeStrings

---
+++ ManejoDeStrings/ManejoDeStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ManejoDeStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Platzi!");
+
+            int height = 170;
+            int age = 20;
+            string name = "Ricardo Celis";
+            string information = "the requested info is: \n User name " + name + "\n user age " 
+                + age + " \n user height " + height;
+            Console.WriteLine(information);
+        }
+    }
+}
