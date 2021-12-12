3d955b287d3d0a794c51e65dbe2bb154509ac163
RetaxMaster <carlos@retaxmaster.com>
2021-12-11 20:40:20 -0600

    Adicion de Operadores Relacionales

---
+++ RelationalOperatorsExample/RelationalOperatorsExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RelationalOperatorsExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value1 = 3;
+            int value2 = 18;
+            int value3 = -18;
+
+            bool result0 = value1 == value2;
+            Console.WriteLine("The result of value1 == value2 is: " + result0);
+
+            bool result1 = value1 != value2;
+            Console.WriteLine("The result of value1 != value2 is: " + result1);
+
+            bool result2 = value1 > value2;
+            Console.WriteLine("The result of value1 > value2 is: " + result2);
+
+            bool result3 = value1 < value2;
+            Console.WriteLine("The result of value1 < value2 is: " + result3);
+
+            bool result4 = value1 >= value2;
+            Console.WriteLine("The result of value1 >= value2 is: " + result4);
+
+            bool result5 = value1 <= value2;
+            Console.WriteLine("The result of value1 <= value2 is: " + result5);
+        }
+    }
+}
