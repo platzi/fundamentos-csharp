2ca3f32b433710c735f8c30b25fcd9a3bc432eb8
RetaxMaster <carlos@retaxmaster.com>
2021-12-11 21:24:54 -0600

    Adicion de Listas

---
+++ ListExampleProject/ListExampleProject/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListExampleProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            List<string> tacoShoppingList = new List<string>();
+
+            tacoShoppingList.Add("Cinco Tacos de suadero");
+            tacoShoppingList.Add("Cuatro Tacos de Tripa");
+            tacoShoppingList.Add("Cinco tacos de pastor");
+            tacoShoppingList.Add("Cuatro Coca Colas");
+
+            for (int i = 0; i < tacoShoppingList.Count; i++)
+            {
+                Console.WriteLine(tacoShoppingList[i]);
+            }
+
+            tacoShoppingList.Remove("Cinco Tacos de suadero");
+
+            for (int i = 0; i < tacoShoppingList.Count; i++)
+            {
+                Console.WriteLine(tacoShoppingList[i]);
+            }
+
+            //tacoShoppingList.RemoveAt(0);
+        }
+    }
+}
