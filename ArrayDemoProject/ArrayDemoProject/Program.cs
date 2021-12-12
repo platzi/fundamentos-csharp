a50269efbad1e7a977ccf3d8f63d95347280edf1
RetaxMaster <carlos@retaxmaster.com>
2021-12-11 21:11:46 -0600

    Adicion de Arreglos o Arrays

---
+++ ArrayDemoProject/ArrayDemoProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArrayDemoProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string[] coffeTypes;
+            float[] coffePrices;
+
+            coffeTypes = new string[] { "Expresso", "Largo", "Filtrado", "Latte" }; //4
+            coffePrices = new float[] { 1.2f,        1.5f,     5.0f,      5.5f };
+
+            coffeTypes[1] = "Lungo";
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine(coffeTypes[i] + "Coffe $" + coffePrices[i]);
+
+        }
+    }
+}
