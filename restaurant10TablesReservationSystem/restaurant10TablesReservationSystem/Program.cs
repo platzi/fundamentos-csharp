9e2e1b622af9d2f1f25ca1f2c81eff5a4ca830da
RetaxMaster <carlos@retaxmaster.com>
2021-12-12 13:19:07 -0600

    Finalizacion del proyecto

--- restaurant10TablesReservationSystem/restaurant10TablesReservationSystem/Program.cs
+++ restaurant10TablesReservationSystem/restaurant10TablesReservationSystem/Program.cs
@@ -11,18 +11,19 @@ namespace restaurant10TablesReservationSystem
     {
         static void Main(string[] args)
         {
-            string[] userNames = new string[10]{"Mike", "", "", "", "", "", "", "", "", ""};
-            int arrayCurrentIndex = 1;
+            string[] userNames = new string[10] { "Pedro", "Robert", "Joan", "Selena", "Mike", "Dani", "Platz", "i", "Celis", "" };
+            int arrayCurrentIndex = 9;
             bool userType;
 
             Console.WriteLine("Welcome to the best restaurant in the world!");
-            while(arrayCurrentIndex < 10)
+            while (arrayCurrentIndex < 10)
             {
-                if (arrayCurrentIndex == 10)
+                /*if (arrayCurrentIndex == 10)
                 {
                     Console.WriteLine("The restaurant is full, try again next year");
                     Environment.Exit(0);
                 }
+                */
                 Console.WriteLine("\n \n Are you a registered user? Write true, or write false to register");
                 userType = Convert.ToBoolean(Console.ReadLine());
                 if (userType == true)
@@ -49,6 +50,14 @@ namespace restaurant10TablesReservationSystem
                     arrayCurrentIndex++;
                 }
             }
+            Console.WriteLine("The restaurant is full, try again next year\n These are the guests to the dinner:");
+            int auxIndex = 0;
+            foreach (string element in userNames)
+            {
+                Console.WriteLine("User number: {0} and user name:{1}", auxIndex + 1, userNames[auxIndex]);
+                auxIndex++;
+            }
+            Environment.Exit(0);
         }
     }
 }
